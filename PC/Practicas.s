9f7665ca67554e9a4789036337dc32c6d6378dab
AlbertoLagoFdez <alu0101562247@ull.edu.es>
2023-02-08 12:25:35 +0000

    cositas

--- PC/Practicas.s
+++ PC/Practicas.s
@@ -2,4 +2,15 @@
 .text
 
 main:
+    li $s0,0
+    li $t2,1000
+    li $t1,1
+bucle:
+    bgt $t1,$t2,fuera
+    add $s0,$s0,$t1
+    addi $t1,1
+    j bucle
+fuera:
+    lli $v0,10
+    syscall
     
