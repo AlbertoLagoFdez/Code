81e81c5c2368c8a568c7d2e2fd17f4f7bde02f7e
AlbertoLagoFdez <alu0101562247@ull.edu.es>
2023-02-08 21:29:55 +0000

    pc

--- PC/ejercicio1.s
+++ PC/ejercicio1.s
@@ -3,4 +3,19 @@
 main:
     li $t1,3
     li $t2,1
-    bgt 
+    bgt $t1,$t2,opcion1
+    move $t3,$t1
+    j siguiente
+opcion1:
+    move $t3,$t2
+     $t3,$t2
+siguiente:
+    bgt $t1,$t2,opcion2
+    move $t4,$t2
+    j siguiente2
+opcion2:
+    move $t4,$t1
+siguiente2:
+    move $t5,$t3,$t4
+    li $v0,10
+    syscall
