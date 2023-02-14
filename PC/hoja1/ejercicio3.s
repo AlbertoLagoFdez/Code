52c203dad222816441f5a034423fda2f5a118688
AlbertoLagoFdez <alu0101562247@ull.edu.es>
2023-02-14 18:29:07 +0000

    pc

--- PC/hoja1/ejercicio3.s
+++ PC/hoja1/ejercicio3.s
@@ -5,12 +5,12 @@ main:
     li $t2,2
     li $t3,1
 
-    sub $v1,$t2,$t3
-    beq $t0,$v1,$t1
-    slt $t4,$t1,$t3
-    sge $t6,$t2,$t3
-    and $t7,$t4,$t6
-    or $t5,$t7,$t6
+    sub $s0,$t2,$t3
+    seq $t4,$s0,$t1
+    slt $t6,$t1,$t3
+    sge $t7,$t2,$t3
+    and $t0,$t6,$t7
+    or $t5,$t0,$t4
 
 final:
     li $v0,10
