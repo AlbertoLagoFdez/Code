e2d10920df8d9c36cf055bf4ba4afd0d44a4e5d0
AlbertoLagoFdez <alu0101562247@ull.edu.es>
2023-02-14 18:15:44 +0000

    pc

--- PC/hoja1/ejercicio3.s
+++ PC/hoja1/ejercicio3.s
@@ -4,6 +4,14 @@ main:
     li $t1,1
     li $t2,2
     li $t3,1
+
+    subi $t2,$t3
+    beq $t0,$t2,$t1
+    slt $t4,$t1,$t3
+    sge $t6,$t2,$t3
+    and $t7,$t4,$t6
+    or $t5,$t7,$t6
+
 final:
     li $v0,10
     syscall
