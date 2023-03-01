66fb4c048de03898bba0b254a00bfcdd77528c7b
AlbertoLagoFdez <alu0101562247@ull.edu.es>
2023-03-01 14:39:34 +0000

    a

--- AyED/vector_t/vector_t.hpp
+++ AyED/vector_t/vector_t.hpp
@@ -5,11 +5,6 @@ using namespace std;
 
 class vector_t
 {
-    private:
-      double *base_;
-      int n_;
-      void build_(void);
-      void destroy_(void);
     public:
     //constructor, copia y destructor
       vector_t(const int n = 0);
@@ -25,18 +20,11 @@ class vector_t
       double operator[] (const int i ) const;
       double operator[] (const int i );
 
-      ostream write(ostream& os)
-      {
-        os << n_ << endl;
-        for(int = 0; i < n_; i++);
-        os << endl;
-        return os;
-      }  
-
-      istream read(istream& is) 
-      {
-        is >> n_;
-        for();
-        return is;
-      }
+      ostream& write (ostream& = cout); 
+      istream& read (istream& = cin);
+    private:
+      double *base_;
+      int n_;
+      void build_(void);
+      void destroy_(void);
 };
