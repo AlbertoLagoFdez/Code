3247a6d64a43795ced93744765a3ef72a40a299a
AlbertoLagoFdez <alu0101562247@ull.edu.es>
2023-04-26 23:06:57 +0100

    mono

--- AyED/practica5/rpn_t.h
+++ AyED/practica5/rpn_t.h
@@ -1,6 +1,6 @@
-// AUTOR: 
-// FECHA: 
-// EMAIL: 
+// AUTOR: Alberto Lago Fernandez
+// FECHA: 26/04/2023
+// EMAIL: alu0101562247@ull.edu.es
 // VERSION: 2.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 5
